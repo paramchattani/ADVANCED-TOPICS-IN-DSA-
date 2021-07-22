2fc0335c0f2143833b4de290c68dc3c19493f72f
paramchattani <37595290+paramchattani@users.noreply.github.com>
2021-07-22 18:22:25 +0530

    Add files via upload

---
+++ dsu.cpp
@@ -0,0 +1,46 @@
+
+void dfs_size(int v, int p) {
+	sz[v] = 1;
+	for (auto u : adj[v]) {
+		if (u != p) {
+			dfs_size(u, v);
+			sz[v] += sz[u];
+		}
+	}
+}
+Code:
+
+void dfs(int v, int p, bool keep) {
+	int Max = -1, bigchild = -1;
+	for (auto u : adj[v]) {
+		if (u != p && Max < sz[u]) {
+			Max = sz[u];
+			bigchild = u;
+		}
+	}
+	for (auto u : adj[v]) {
+		if (u != p && u != bigchild) {
+			dfs(u, v, 0);
+		}
+	}
+	if (bigchild != -1) {
+		dfs(bigchild, v, 1);
+		swap(vec[v], vec[bigchild]);
+	}
+	vec[v].push_back(v);
+	cnt[color[v]]++;
+	for (auto u : adj[v]) {
+		if (u != p && u != bigchild) {
+			for (auto x : vec[u]) {
+				cnt[color[x]]++;
+				vec[v].push_back(x);
+			}
+		}
+	}
+	// there are cnt[c] vertex in subtree v color with c
+	if (keep == 0) {
+		for (auto u : vec[v]) {
+			cnt[color[u]]--;
+		}
+	}
+}
